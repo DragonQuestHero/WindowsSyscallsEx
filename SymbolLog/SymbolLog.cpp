843bd069ea4f3e8fb5d7189cae31a374f087373a
新手村的满级史莱姆 <DragonQuestMaou@gmail.com>
2021-09-10 17:55:17 +0800

    #Comment Init

---
+++ SymbolLog/SymbolLog.cpp
@@ -0,0 +1,63 @@
+﻿#include <windows.h>
+#include <dbghelp.h>
+#include <Psapi.h>
+
+
+#include <iostream>
+#include <fstream>
+
+std::fstream log_file;
+
+BOOL PsymEnumeratesymbolsCallback(
+    PSYMBOL_INFO pSymInfo,
+    ULONG SymbolSize,
+    PVOID UserContext
+)
+{
+    log_file << pSymInfo->Name << ":"; 
+    //log_file <<std::hex<< pSymInfo->Address << ":" << pSymInfo->Address - pSymInfo->ModBase << std::endl;
+    log_file << std::hex << ": 0x" << pSymInfo->Address - pSymInfo->ModBase << std::endl;
+    return TRUE;
+}
+
+
+int main()
+{
+    SymSetOptions(SYMOPT_LOAD_LINES | SYMOPT_FAVOR_COMPRESSED);
+    if (!SymInitialize(GetCurrentProcess(), NULL, FALSE))
+    {
+        std::cout << "error";
+        getchar();
+    }
+
+    OFSTRUCT ReOpenBuff;
+    HANDLE file_handle = (HANDLE)OpenFile("C:\\Windows\\System32\\ntoskrnl.exe", &ReOpenBuff, OF_READ);
+
+
+    DWORD Needed;
+	ULONG64 Base[1024];
+    EnumDeviceDrivers((LPVOID*)Base, sizeof(Base), &Needed);
+
+    ULONG64 base_addr = SymLoadModuleEx(GetCurrentProcess(), file_handle, "C:\\Windows\\System32\\ntoskrnl.exe", nullptr, Base[0], 0, 0, 0);
+
+
+    log_file.open("log.txt",std::ios::out);
+
+    if (!SymEnumSymbols(GetCurrentProcess(), Base[0], "*!*", &PsymEnumeratesymbolsCallback, NULL))
+    {
+        std::cout << "error";
+        getchar();
+    }
+
+
+
+
+
+
+
+
+
+
+    system("pause");
+}
+
